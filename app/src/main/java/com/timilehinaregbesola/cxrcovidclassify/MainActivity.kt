0cd49b377d481c713cde917bd37525be2e56cb2d
Timilehin <aregbestimi@gmail.com>
2021-06-17 01:42:02 +0100

    Initial commit

---
+++ app/src/main/java/com/timilehinaregbesola/cxrcovidclassify/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.timilehinaregbesola.cxrcovidclassify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.timilehinaregbesola.cxrcovidclassify.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
