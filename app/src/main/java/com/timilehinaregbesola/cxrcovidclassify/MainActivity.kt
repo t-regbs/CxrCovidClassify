2a3449fec3ee5b76209d32ecec83a4c06637b696
Timilehin <aregbestimi@gmail.com>
2021-07-17 06:27:40 +0100

    add permissions and remove unused files

--- app/src/main/java/com/timilehinaregbesola/cxrcovidclassify/MainActivity.kt
+++ app/src/main/java/com/timilehinaregbesola/cxrcovidclassify/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.timilehinaregbesola.cxrcovidclassify
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -10,6 +12,8 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.timilehinaregbesola.cxrcovidclassify.navigation.NavGraph
 import com.timilehinaregbesola.cxrcovidclassify.ui.CxrCovidClassifyTheme
@@ -20,6 +24,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this)[UploadViewModel::class.java]
         window.makeTransparentStatusBar()
+        if (allPermissionsGranted()) {
+            setViewContent()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun setViewContent() {
         setContent {
             CxrCovidClassifyTheme {
                 window.statusBarColor = MaterialTheme.colors.background.toArgb()
@@ -32,6 +46,12 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(this, ClassifierActivity::class.java))
     }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun Window.makeTransparentStatusBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
@@ -41,5 +61,10 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
             )
         }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 1001
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 }
